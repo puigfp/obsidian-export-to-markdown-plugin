875d8698ef6a9c3191b78c7651ecd509b1e58c1f
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2025-08-24 14:48:41 +0200

    Add settings and cleanup main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-import { assert } from "console";
 import {
 	App,
-	Editor,
 	MarkdownView,
 	Modal,
 	Notice,
@@ -10,111 +8,85 @@ import {
 	Setting,
 } from "obsidian";
 import { parseMarkdown, stringifyMarkdownTree } from "./src/parse";
-import * as mdast from "mdast";
-import { computeNewLocalPaths, convertLinksToPaths, extractLinksFromTree, updateTreeLinks } from "src/transform";
+import {
+	computeNewLocalPaths,
+	convertLinksToPaths,
+	extractLinksFromTree,
+	updateTreeLinks,
+} from "src/transform";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ExportToMarkdownPlugingSettings {
+	attachmentFolderName: string;
+	exportFolderName: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
+const DEFAULT_SETTINGS: ExportToMarkdownPlugingSettings = {
+	attachmentFolderName: "attachments",
+	exportFolderName: "markdown-export-output",
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ExportToMarkdownPlugin extends Plugin {
+	settings: ExportToMarkdownPlugingSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: "export-to-markdown",
+			id: "export-current-file-to-markdown",
 			name: "Export current file",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
 				const markdownView =
 					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (checking) {
 					return markdownView !== undefined;
 				}
-				assert(markdownView !== undefined);
-				const markdownFile = markdownView?.file;
-				if (markdownFile == undefined || markdownFile == null) {
-					return;
-				}
-				// read file
+				const markdownFile = markdownView?.file!;
+
 				(async () => {
 					const content = await this.app.vault.read(markdownFile);
 					const tree = parseMarkdown(content);
 					const links = extractLinksFromTree(tree);
-					const linksToFiles = convertLinksToPaths(markdownFile, links, this.app);
-					const attachmentFolderName = "attachments";
-					const filesToCopy = computeNewLocalPaths(linksToFiles, attachmentFolderName);
+					const linksToFiles = convertLinksToPaths(
+						markdownFile,
+						links,
+						this.app
+					);
+					const attachmentFolderName =
+						this.settings.attachmentFolderName;
+					const filesToCopy = computeNewLocalPaths(
+						linksToFiles,
+						attachmentFolderName
+					);
 					const updatedTree = updateTreeLinks(tree, filesToCopy);
 					const stringifiedTree = stringifyMarkdownTree(updatedTree);
-					const outputFolder = `markdown-export-output/${markdownFile.basename}`;
+					const outputFolder = `${this.settings.exportFolderName}/${markdownFile.basename}`;
 					const outputPath = `${outputFolder}/${markdownFile.name}`;
 					if (!this.app.vault.getAbstractFileByPath(outputFolder)) {
 						await this.app.vault.createFolder(outputFolder);
 					}
 					await this.app.vault.create(outputPath, stringifiedTree);
 					const attachmentsFolder = `${outputFolder}/${attachmentFolderName}`;
-					if (!this.app.vault.getAbstractFileByPath(attachmentsFolder)) {
+					if (
+						!this.app.vault.getAbstractFileByPath(attachmentsFolder)
+					) {
 						await this.app.vault.createFolder(attachmentsFolder);
 					}
 					for (const fileToCopy of filesToCopy.values()) {
-						const file = this.app.vault.getFileByPath(fileToCopy.localPath)!;
-						await this.app.vault.copy(file, `${outputFolder}/${fileToCopy.exportedLocalPath}`);
+						const file = this.app.vault.getFileByPath(
+							fileToCopy.localPath
+						)!;
+						await this.app.vault.copy(
+							file,
+							`${outputFolder}/${fileToCopy.exportedLocalPath}`
+						);
 					}
 				})();
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-		// 	console.log("click", evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
+		this.addSettingTab(new ExportToMarkdownSettingTab(this.app, this));
 	}
 
 	onunload() {}
@@ -148,10 +120,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ExportToMarkdownSettingTab extends PluginSettingTab {
+	plugin: ExportToMarkdownPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ExportToMarkdownPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -162,14 +134,41 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
+			.setName("Export folder name")
+			.setDesc(
+				"The name of the folder where exported markdown files will be stored"
+			)
 			.addText((text) =>
 				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
+					.setValue(this.plugin.settings.exportFolderName)
 					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+						if (value.length === 0) {
+							new Notice(
+								"Export folder name cannot be empty"
+							);
+							return;
+						}
+						this.plugin.settings.exportFolderName = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Attachment folder name")
+			.setDesc(
+				"The name of the folder where attachments will be stored alongside the exported markdown file"
+			)
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.attachmentFolderName)
+					.onChange(async (value) => {
+						if (value.length === 0) {
+							new Notice(
+								"Attachment folder name cannot be empty"
+							);
+							return;
+						}
+						this.plugin.settings.attachmentFolderName = value;
 						await this.plugin.saveSettings();
 					})
 			);
